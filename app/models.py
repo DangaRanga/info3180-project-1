d553c14e51e7e15eb5376540bdaa5f5b2e75e837
DangaRanga <jasonblue1626@gmail.com>
2021-03-22 19:25:17 -0500

    Completed step 1/non-heroku segment

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
 from . import db
+import enum
+from sqlalchemy import Integer, Enum
 
 
-class Property(db.Model):
+class PropertyTypes(enum.Enum):
+    """Defines the property types."""
+    apartment = 'apartment'
+    house = 'house'
+
+
+class PropertyModel(db.Model):
     """The model for a housing property."""
 
     __tablename__ = 'properties'
@@ -10,11 +18,25 @@ class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     no_bedrooms = db.Column(db.Integer)
+    description = db.Column(db.String(500))
     no_bathrooms = db.Column(db.Integer)
     location = db.Column(db.String(100))
     price = db.Column(db.Numeric(10, 2))
     photo_path = db.Column(db.String(255))
+    property_type = db.Column(Enum(PropertyTypes))
 
     # Initializing the Model, **kwargs is used to accept all arguments given
-    def __init__(self, **kwargs):
-        super(Property, self).__init__(**kwargs)
+    def __init__(
+            self, title, no_bedrooms, no_bathrooms, description,
+            price, location, property_type, photo_path):
+
+        super().__init__()
+
+        self.title = title
+        self.no_bedrooms = no_bedrooms
+        self.no_bathrooms = no_bathrooms
+        self.description = description
+        self.price = price
+        self.location = location
+        self.property_type = property_type
+        self.photo_path = photo_path
--- app/views.py
+++ app/views.py
@@ -4,14 +4,33 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
+import os
 
-from app import app
-from flask import render_template, request, redirect, url_for
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from flask.helpers import send_from_directory
+
+from app.forms import PropertyForm
+from app.models import PropertyModel
+
+
+# Utility functions
+def get_uploaded_images():
+    """Retrieves uploaded images
+    Args:
+        None
+    Returns:
+        <list> A list of the files in the upload folder
+    """
+    upload_dir = app.config.get('UPLOAD_FOLDER')
+    return sorted(os.listdir(upload_dir))
 
 
 ###
 # Routing for your application.
 ###
+
 
 @app.route('/')
 def home():
@@ -25,16 +44,76 @@ def about():
     return render_template('about.html', name="Mary Jane")
 
 
-@app.route('/property')
-def add_propery():
+@app.route('/property', methods=['GET', 'POST'])
+def add_property():
     """Form for adding properties"""
-    pass
 
-###
-# The functions below should be applicable to all Flask apps.
-###
+    # Initialize the property form
+    form = PropertyForm()
 
-# Display Flask WTF errors as Flash messages
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            # Extract form data
+            property_title = form.property_title.data
+            description = form.description.data
+            no_bedrooms = form.no_rooms.data
+            no_bathrooms = form.no_bathrooms.data
+            price = form.price.data
+            location = form.location.data
+            property_type = form.property_type.data
+            photo = form.photo.data
+
+            # Save the photo
+            photo_path = secure_filename(photo.filename)
+            path_joined = os.path.join(app.config['UPLOAD_FOLDER'], photo_path)
+            print(path_joined)
+            photo.save(path_joined)
+
+            # Store the property to the database
+            property_model = PropertyModel(
+                title=property_title,
+                description=description,
+                no_bedrooms=no_bedrooms,
+                no_bathrooms=no_bathrooms,
+                price=price,
+                location=location,
+                property_type=property_type,
+                photo_path=photo_path)
+
+            db.session.add(property_model)
+            db.session.commit()
+
+            # Redirect to properties page
+            flash('Property successfully saved', 'success')
+            return redirect(url_for('get_properties'))
+        flash_errors(form)
+    return render_template('property.html', form=form)
+
+
+@app.route('/properties')
+def get_properties():
+    properties = PropertyModel.query.all()
+    return render_template('properties.html', properties=properties)
+
+
+@app.route("/property/<property_id>")
+def view_property(property_id):
+    # Retrieve the property with the matching id
+    prop = PropertyModel.query.filter_by(id=property_id).first()
+    return render_template("view_property.html", prop=prop)
+
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    upload_dir = app.config.get('UPLOAD_FOLDER')
+    print(upload_dir)
+    return send_from_directory(upload_dir, filename=filename)
+
+    ###
+    # The functions below should be applicable to all Flask apps.
+    ###
+
+    # Display Flask WTF errors as Flash messages
 
 
 def flash_errors(form):
